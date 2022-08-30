6e18bdeaac7b2e55797b22132022206168fc624b
Eduardo Alves <ealves@me.com>
2022-08-29 21:09:40 -0300

    Testing scanner and inputs.

--- JavaTest.java
+++ JavaTest.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class JavaTest {
 // main method
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String name= "World";
+        if (args.length > 0) {
+            name = args[0];
+        }
+        System.out.println("Hello, "+ name + "! Type something fun:");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("You entered: " + input);
     }
 }
